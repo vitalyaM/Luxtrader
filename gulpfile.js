4295f0e8b158f36b1f8e49ea3e49fd6c152c1905
ambulancer <45568410+vitalyaM@users.noreply.github.com>
2020-03-19 09:11:51 +0300

    slider

--- gulpfile.js
+++ gulpfile.js
@@ -1,67 +1,72 @@
-var gulp = require('gulp'),
-	sass = require('gulp-sass'),
-	browserSync = require('browser-sync'),
-	concat = require('gulp-concat'),
-	uglify = require('gulp-uglify'),
-	cleanCSS = require('gulp-clean-css'),
-	rename = require('gulp-rename'),
-	autoprefixer = require('gulp-autoprefixer'),
-	notify = require("gulp-notify"),
-	kit = require('gulp-kit');
+var gulp = require("gulp"),
+  sass = require("gulp-sass"),
+  browserSync = require("browser-sync"),
+  concat = require("gulp-concat"),
+  uglify = require("gulp-uglify"),
+  cleanCSS = require("gulp-clean-css"),
+  rename = require("gulp-rename"),
+  autoprefixer = require("gulp-autoprefixer"),
+  notify = require("gulp-notify"),
+  kit = require("gulp-kit");
 
 // Сервер и автообновление страницы Browsersync
-gulp.task('browser-sync', function () {
-	browserSync({
-		server: {
-			baseDir: 'app'
-		},
-		notify: false,
-		// online: false, // Work offline without internet connection
-		// tunnel: true,
-		// tunnel: "projectmane", //Demonstration page: http://projectmane.localtunnel.me
-	});
+gulp.task("browser-sync", function() {
+  browserSync({
+    server: {
+      baseDir: "app"
+    },
+    notify: false
+    // online: false, // Work offline without internet connection
+    // tunnel: true,
+    // tunnel: "projectmane", //Demonstration page: http://projectmane.localtunnel.me
+  });
 });
 
 // Минификация пользовательских скриптов проекта и JS библиотек в один файл
-gulp.task('js', function () {
-	return gulp.src([
-		//'app/libs/jquery/dist/jquery.min.js',
-		//'node_modules/jquery/dist/jquery.min.js', // npm vendor example (npm i --save-dev jquery)
-		'app/js/source/main.js', // Всегда в конце
-	])
-		.pipe(concat('main.min.js'))
-		.pipe(uglify()) // Минимизировать весь js (на выбор)
-		.pipe(gulp.dest('app/js'))
-		.pipe(browserSync.reload({ stream: true }));
+gulp.task("js", function() {
+  return gulp
+    .src([
+      //'app/libs/jquery/dist/jquery.min.js',
+      //'node_modules/jquery/dist/jquery.min.js', // npm vendor example (npm i --save-dev jquery)
+      "app/js/source/main.js" // Всегда в конце
+    ])
+    .pipe(concat("main.min.js"))
+    .pipe(uglify()) // Минимизировать весь js (на выбор)
+    .pipe(gulp.dest("app/js"))
+    .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('sass', function () {
-	return gulp.src('app/scss/**/*.scss')
-		.pipe(sass({ outputStyle: 'expand' }).on("error", notify.onError()))
-		.pipe(rename({ suffix: '.min', prefix: '' }))
-		.pipe(autoprefixer(['last 5 versions']))
-		.pipe(cleanCSS()) // Опционально, закомментировать при отладке
-		.pipe(gulp.dest('app/css'))
-		.pipe(browserSync.reload({ stream: true }));
+gulp.task("sass", function() {
+  return gulp
+    .src("app/scss/**/*.scss")
+    .pipe(sass({ outputStyle: "expand" }).on("error", notify.onError()))
+    .pipe(rename({ suffix: ".min", prefix: "" }))
+    .pipe(autoprefixer(["last 5 versions"]))
+    .pipe(cleanCSS()) // Опционально, закомментировать при отладке
+    .pipe(gulp.dest("app/css"))
+    .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('codeKit', function () {
-	return gulp.src('app/kit/**/*.kit')
-		.pipe(kit())
-		.pipe(gulp.dest('app/'))
-		.pipe(browserSync.reload({ stream: true }));
+gulp.task("codeKit", function() {
+  return gulp
+    .src("app/kit/**/*.kit")
+    .pipe(kit())
+    .pipe(gulp.dest("app/"))
+    .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('code', function () {
-	return gulp.src('app/**/*.html')
-		.pipe(browserSync.reload({ stream: true }))
+gulp.task("code", function() {
+  return gulp.src("app/**/*.html").pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('watch', function () {
-	gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'));
-	gulp.watch(['libs/**/*.js', 'app/js/main.js'], gulp.parallel('js'));
-	gulp.watch('app/kit/**/*.kit', gulp.parallel('codeKit'));
-	gulp.watch('app/*.html', gulp.parallel('code'));
+gulp.task("watch", function() {
+  gulp.watch("app/scss/**/*.scss", gulp.parallel("sass"));
+  gulp.watch(["libs/**/*.js", "app/js/main.js"], gulp.parallel("js"));
+  gulp.watch("app/kit/**/*.kit", gulp.parallel("codeKit"));
+  gulp.watch("app/*.html", gulp.parallel("code"));
 });
 
-gulp.task('default', gulp.parallel('sass', 'js', 'codeKit', 'browser-sync', 'watch'));
+gulp.task(
+  "default",
+  gulp.parallel("sass", "js", "codeKit", "browser-sync", "watch")
+);
